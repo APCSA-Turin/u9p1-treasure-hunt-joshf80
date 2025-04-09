aa2fe96ffdc08250be6c803045d89191397d5786
Joshua <139407249+joshf80@users.noreply.github.com>
2025-04-09 05:07:20 -0400

    Update Trophy.java

--- src/main/java/com/example/project/Trophy.java
+++ src/main/java/com/example/project/Trophy.java
@@ -1,7 +1,7 @@
 package com.example.project;
 
 
-public class Trophy extends Sprite { //child of trophy
+public class Trophy extends Treasure { //child of treasure
     public Trophy(int x, int y) {
         super(x, y);
     }
